7d5d41674c8825261c02c6fb23e4084f2c2ade4a
AryaG-06 <aryaghatole1@gmail.com>
2026-09-04 12:13:23 +0900

    ProcessWorkbook.py

--- process_workbook.py
+++ process_workbook.py
@@ -20,4 +20,4 @@ def process_workbook(filename, percentage):
 
 
 percent=int(input("Enter Price Reduction Percentage: "))
-process_workbook("transactions.xlsx", percent)
+process_workbook("SalesList.xlsx", percent)
